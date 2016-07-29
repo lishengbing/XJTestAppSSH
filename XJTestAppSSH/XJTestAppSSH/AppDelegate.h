eb80741919c7121bee62616857714c43606e8b40
lishengbing <1269113083@qq.com>
2016-07-29 14:53:55 +0800

    初始化项目

---
+++ XJTestAppSSH/XJTestAppSSH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XJTestAppSSH
+//
+//  Created by 李胜兵 on 16/7/29.
+//  Copyright © 2016年 李胜兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XJTestAppSSH/XJTestAppSSH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJTestAppSSH
+//
+//  Created by 李胜兵 on 16/7/29.
+//  Copyright © 2016年 李胜兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
