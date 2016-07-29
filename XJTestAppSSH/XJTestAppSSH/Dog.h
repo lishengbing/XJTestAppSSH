2ce10c017e86ca0a27c657eebf27e058c672bb45
lishengbing <1269113083@qq.com>
2016-07-29 14:55:14 +0800

    增加Dog类

---
+++ XJTestAppSSH/XJTestAppSSH/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  XJTestAppSSH
+//
+//  Created by 李胜兵 on 16/7/29.
+//  Copyright © 2016年 李胜兵. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ XJTestAppSSH/XJTestAppSSH/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  XJTestAppSSH
+//
+//  Created by 李胜兵 on 16/7/29.
+//  Copyright © 2016年 李胜兵. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
